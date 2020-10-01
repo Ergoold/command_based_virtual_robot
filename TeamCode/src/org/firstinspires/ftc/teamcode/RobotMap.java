47c323116332e9f56e963288122baf99c082eef4
Ergoold <eran1gold@gmail.com>
2020-10-02 00:25:52 +0300

    create RobotMap

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/RobotMap.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * The RobotMap is a mapping from the names of sensors and actuators into to a variable.
+ *
+ * <p>Keeping the entire mapping in one place like this allows us to easily change the name of any sensor or actuator
+ * without affecting the rest of the code.</p>
+ */
+public interface RobotMap {
+    // For example to map the left and right motors, you could define the following variables to use with your
+    // drivetrain subsystem.
+    // int LEFT_MOTOR = "left_motor";
+    // int RIGHT_MOTOR = "right_motor";
+}

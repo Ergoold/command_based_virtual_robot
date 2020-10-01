e3b3488dd95b23142efc3a23bfdb1193806c3784
Ergoold <eran1gold@gmail.com>
2020-10-02 00:28:21 +0300

    added ExampleSubsystem

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/subsystems/ExampleSubsystem.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.spikes2212.ftc.command.Subsystem;
+
+/**
+ * An example subsystem. Replace me with your own subsystem.
+ */
+public class ExampleSubsystem implements Subsystem {
+    // Put methods for controlling this subsystem here. Call these from Commands.
+}

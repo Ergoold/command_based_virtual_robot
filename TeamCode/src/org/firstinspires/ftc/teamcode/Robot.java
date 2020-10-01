e077383c13525d9a6a83032a27613a69e82df252
Ergoold <eran1gold@gmail.com>
2020-10-02 00:20:24 +0300

    create Robot.java

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * This class is where the subsystems of the robot live.
+ */
+public class Robot {
+    // The robot's subsystems are defined here...
+
+    /**
+     * An empty constructor that initializes all of the subsystems.
+     */
+    public Robot() {
+
+    }
+}

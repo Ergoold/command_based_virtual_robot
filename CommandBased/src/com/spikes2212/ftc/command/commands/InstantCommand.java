b51bccc49540ac78880a6939074465c3aacbf25f
Ergoold <eran1gold@gmail.com>
2020-10-02 22:15:09 +0300

    created InstantCommand

---
+++ CommandBased/src/com/spikes2212/ftc/command/commands/InstantCommand.java
@@ -0,0 +1,23 @@
+package com.spikes2212.ftc.command.commands;
+
+import com.spikes2212.ftc.command.Command;
+import com.spikes2212.ftc.command.Subsystem;
+
+public class InstantCommand extends Command {
+    Runnable runnable;
+
+    public InstantCommand(Runnable runnable, Subsystem... requirements) {
+        this.runnable = runnable;
+        require(requirements);
+    }
+
+    @Override
+    public void init() {
+        runnable.run();
+    }
+
+    @Override
+    public boolean isDone() {
+        return true;
+    }
+}

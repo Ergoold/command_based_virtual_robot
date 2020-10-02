017d32cdb750f62789977fd014cff90d7f1f3a12
Ergoold <eran1gold@gmail.com>
2020-10-02 23:46:41 +0300

    create RaceCommandGroup

---
+++ CommandBased/src/com/spikes2212/ftc/command/groups/RaceCommandGroup.java
@@ -0,0 +1,41 @@
+package com.spikes2212.ftc.command.groups;
+
+import com.spikes2212.ftc.command.Command;
+
+import java.util.*;
+
+public class RaceCommandGroup extends CommandGroup {
+
+    public RaceCommandGroup(Command... commands) {
+        super(new ArrayList<>(new HashSet<>(Arrays.asList(commands))));
+    }
+
+    @Override
+    public void init() {
+        for (Command command : commands) {
+            command.init();
+        }
+    }
+
+    @Override
+    public void exec() {
+        for (Command command : commands) {
+            command.exec();
+        }
+    }
+
+    @Override
+    public boolean isDone() {
+        for (Command command : commands) {
+            if (command.isDone()) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void end(boolean done) {
+        for (Command command : commands) {
+            command.end(command.isDone());
+        }
+    }
+}
